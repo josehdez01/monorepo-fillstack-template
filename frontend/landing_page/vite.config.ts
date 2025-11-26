0f7be8add94c5c785d292ce546f986290b7491e0
Chema <josehdez01@gmail.com>
2025-11-25 20:44:32 -0800

    Lint + tailwind config issue

--- frontend/landing_page/vite.config.ts
+++ frontend/landing_page/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [react(), tanstackRouter()],
+    plugins: [react(), tanstackRouter(), tailwindcss()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
